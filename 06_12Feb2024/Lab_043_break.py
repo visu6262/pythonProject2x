f86f29217f1c922349f6200dc2f7c610c99fee12
visu6262 <[s.viswanath9@yahoo.com]>
2024-02-22 00:15:21 +0530

    12 feb 2024 class

---
+++ 06_12Feb2024/Lab_043_break.py
@@ -0,0 +1,14 @@
+# break
+for s in range(1,11):
+    if s==7:
+        break
+    print(s)
+print("out of if loop")
+"""---------------------"""
+print("type 2")
+for s in range(1,11):
+    print(s)
+    if s==7:
+        break
+    # print(s)
+print("out of if loop")
---
+++ 06_12Feb2024/Lab_044_pass.py
@@ -0,0 +1,13 @@
+# pass
+for s in range(1, 6):
+    if s == 3:
+        pass
+    else:
+        print(s)
+print("out of if loop")
+"""--------------------"""
+for xx in range(1, 11):
+    if xx == 5:
+        break
+    else:
+        print(xx)
