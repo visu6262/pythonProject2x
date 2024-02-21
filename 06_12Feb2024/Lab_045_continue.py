f0f3b2cc100c98d89daf9244f81cfc10b1320760
visu6262 <[s.viswanath9@yahoo.com]>
2024-02-22 00:17:36 +0530

    12 feb 2024 class

---
+++ 06_12Feb2024/Lab_045_continue.py
@@ -0,0 +1,14 @@
+# continue
+# for x in range(1, 11):
+#     if x % 2 == 0:
+#         print("even number: ", x)
+#     else:
+#         print("odd number:", x)
+"""-----------------"""
+print("type 2____________")
+
+for x in range(1, 11):
+    if x % 2 == 0:
+        print("even number: ", x)
+    continue
+    print("odd number:", x)
---
+++ 06_12Feb2024/Lab_046.py
@@ -0,0 +1,6 @@
+for num in range(1,11):
+    if num % 3 == 0:
+        print(num)
+    else:
+        print(num * 2)
+    print("hello python")
