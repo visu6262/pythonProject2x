5d20683c9cea7e440c010439c3c96858c11ebb03
visu6262 <[s.viswanath9@yahoo.com]>
2024-03-07 00:33:59 +0530

    19 feb 2024 class

--- 09_19Feb2024/Lab_084.py
+++ 09_19Feb2024/Lab_084.py
@@ -3,13 +3,13 @@ class CAR():
     modal = None
 
     def mod_col(self):
-        print("You are car colour ", self.colour, "and car model is:", self.modal)
+        print("You are car colour :", self.colour,"\nand car model is:", self.modal)
 
 x = input("enter car colour:")
 y = input("enter car modal:")
 
 
 a = CAR()
-a.carmodal=x
-a.carcolour=y
+a.modal=x
+a.colour=y
 a.mod_col()
